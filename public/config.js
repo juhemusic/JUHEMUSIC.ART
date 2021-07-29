ed0fc6d5077ac9209d0544cd45a49953cc32e5a9
juhemusic <88114616+juhemusic@users.noreply.github.com>
2021-07-29 13:57:07 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,24 +2,24 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: 'JUHEMUSIC.ART 服务状态',
 
   // 站点链接
-  SiteUrl: '/',
+  SiteUrl: 'https://status.juhemusic.art/',
 
   // UptimeRobot Api 域名
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
   // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
+  ApiDomain: 'https://status.juhemusic.art/',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u1388687-ff5483a554c1bef88dc5b307',
+    'm788820365-8b1a5ea9d700443067ea6546',
+    'm788821163-05e3ac6270e220adacea304b',
+    'm788821106-23d29714ff2a141bf253c03f',
   ],
 
   // 是否显示监测站点的链接
@@ -33,8 +33,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '首页',
+      url: 'https://status.juhemusic.art/'
     },
     {
       text: 'GitHub',
